94424712c1428870facf1023ee4201efe9f53845
Piyush026 <piyushjaiswal026@gmail.com>
2025-02-15 17:30:17 +0530

    model training completed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from wineQualitymlProject import logger
 from wineQualitymlProject.pipeline.data_ingestion_stage import DataIngestionTrainingPipeline
+from wineQualitymlProject.pipeline.data_transformation_stage import DataTransformationTrainingPipeline
 from wineQualitymlProject.pipeline.data_validation_stage import DataValidationTrainingPipeline
+from wineQualitymlProject.pipeline.model_trainer_stage import ModelTrainerTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -18,6 +20,27 @@ STAGE_NAME = "Data Validation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Model Trainer stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelTrainerTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
