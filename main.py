b8ef169ba7e23615fa005248138c147928f015f9
Piyush026 <piyushjaiswal026@gmail.com>
2025-02-15 16:06:17 +0530

    data validation completed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from wineQualitymlProject import logger
 from wineQualitymlProject.pipeline.data_ingestion_stage import DataIngestionTrainingPipeline
+from wineQualitymlProject.pipeline.data_validation_stage import DataValidationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -12,3 +13,13 @@ except Exception as e:
         logger.exception(e)
         raise e
 
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
