c5aa7567064bea8d400cada5399c189714abb961
Piyush026 <piyushjaiswal026@gmail.com>
2025-02-16 15:55:24 +0530

    model evaluation completed

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from wineQualitymlProject.pipeline.data_ingestion_stage import DataIngestionTrai
 from wineQualitymlProject.pipeline.data_transformation_stage import DataTransformationTrainingPipeline
 from wineQualitymlProject.pipeline.data_validation_stage import DataValidationTrainingPipeline
 from wineQualitymlProject.pipeline.model_trainer_stage import ModelTrainerTrainingPipeline
+from wineQualitymlProject.pipeline.model_evaluation_stage import ModelEvaluationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -41,6 +42,17 @@ STAGE_NAME = "Model Trainer stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = ModelTrainerTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Model evaluation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelEvaluationTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
